976270eb97bf390d114750dbe50e8040a5b8ab9c
devkartic <dev.kartic@gmail.com>
2020-11-23 22:54:16 +0600

    Static data retrive from view

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Khokon',
+        'title': 'First post',
+        'description': 'First post description',
+        'date_posted': 'November 20, 2020'
+    },
+    {
+        'author': 'Kartic',
+        'title': 'Second post',
+        'description': 'Second post description',
+        'date_posted': 'November 22, 2020'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    contest = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', contest)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'Title Page'})
