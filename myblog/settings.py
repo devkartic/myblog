4c8c71481b5af00a4bf94c5aa95cc7d6166a7072
devkartic <dev.kartic@gmail.com>
2020-12-22 23:34:01 +0600

    add crispy_forms tag

--- myblog/settings.py
+++ myblog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
